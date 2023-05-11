c805019d5e30d5c8d8b6c0aa7627df97ffe01701
DevDidier <133176640+DevDidier@users.noreply.github.com>
2023-05-10 23:10:54 -0500

    didier
    final jornada 2023-05-10

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
             this.navbar.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,52 +44,59 @@
             | System.Windows.Forms.AnchorStyles.Left)));
             this.navbar.BackgroundImage = global::BibliotecaApp.Properties.Resources.fondo_amarillo;
             this.navbar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.navbar.Controls.Add(this.button4);
             this.navbar.Controls.Add(this.button1);
             this.navbar.Controls.Add(this.button3);
             this.navbar.Controls.Add(this.button2);
             this.navbar.Controls.Add(this.panel3);
             this.navbar.Location = new System.Drawing.Point(-4, 0);
             this.navbar.Name = "navbar";
-            this.navbar.Size = new System.Drawing.Size(206, 454);
+            this.navbar.Size = new System.Drawing.Size(233, 536);
             this.navbar.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button1.BackgroundImage = global::BibliotecaApp.Properties.Resources.blackandwhite;
+            this.button1.AutoEllipsis = true;
+            this.button1.AutoSize = true;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(3, 134);
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(16, 158);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(200, 58);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Ingresar";
+            this.button1.Text = "Ingresar Libro";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button3
             // 
             this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button3.Location = new System.Drawing.Point(3, 321);
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(16, 286);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(200, 58);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "Ingresar Salidas";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
             this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button2.BackgroundImage = global::BibliotecaApp.Properties.Resources.fondo_ladrillos;
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button2.Location = new System.Drawing.Point(3, 226);
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(16, 222);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(200, 58);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Text = "Catalogo Libros";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // panel3
@@ -97,9 +105,9 @@
             this.panel3.BackColor = System.Drawing.Color.Transparent;
             this.panel3.BackgroundImage = global::BibliotecaApp.Properties.Resources.baner_u;
             this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel3.Location = new System.Drawing.Point(3, -42);
+            this.panel3.Location = new System.Drawing.Point(16, -21);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(203, 170);
+            this.panel3.Size = new System.Drawing.Size(203, 173);
             this.panel3.TabIndex = 0;
             // 
             // panel1
@@ -109,17 +117,30 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.AutoSize = true;
             this.panel1.BackgroundImage = global::BibliotecaApp.Properties.Resources.fondo_ladrillos;
-            this.panel1.Location = new System.Drawing.Point(197, 0);
+            this.panel1.Location = new System.Drawing.Point(196, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(613, 454);
+            this.panel1.Size = new System.Drawing.Size(655, 536);
             this.panel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button4.Location = new System.Drawing.Point(16, 350);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 58);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Salidas";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(806, 450);
+            this.ClientSize = new System.Drawing.Size(849, 532);
             this.Controls.Add(this.navbar);
             this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
@@ -127,6 +148,7 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.navbar.ResumeLayout(false);
+            this.navbar.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,6 +162,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
